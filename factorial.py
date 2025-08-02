399e6df94a74e74a57ba0f4c02b71df6e9e8fc8a
Dinasha <dinashaper@gmail.com>
2025-08-02 20:12:17 +1200

    factorial
    factorial

---
+++ factorial.py
@@ -0,0 +1,25 @@
+
+def factorial(number):
+    if (number < 0):
+        return("Value should not be negative")
+
+    elif (0 <= number <= 1):
+        return (1)
+    
+    else :
+        fact = 1
+        for i in range(1, number+1):
+            fact *= i
+        return(fact)
+
+
+if __name__ == "__main__":
+    number = int(input("Enter number:"))
+    result = factorial(number)
+    print(result)
+
+
+
+        
+
+    
